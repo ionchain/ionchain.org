36dbe39d3e394a05c11dba0976ee1daea22fd997
cssshine <cssshine06@gmail.com>
2018-09-04 17:57:13 +0800

    上一次修改遗漏

--- dist/app/dest/base/currency.js
+++ dist/app/dest/base/currency.js
@@ -350,17 +350,16 @@
     });
     // 移动端footer部分
     $(".footer_nav>li").click(function(){
-    // console.log(".footer_nav>li");
-    if($(this).children(".drop_down").hasClass("drop_down_b")){
-        $(this).children(".drop_down").removeClass("drop_down_b");
-        $(this).children("div").css("border-bottom","1px solid #3D3F4A");
-        $(this).removeClass("img_xz");
-    }else{
-        $(this).children(".drop_down").addClass("drop_down_b").parent().siblings().children(".drop_down").removeClass("drop_down_b");;
-        $(this).children("div").css("border-bottom","transparent");
-        $(this).addClass("img_xz").siblings().removeClass("img_xz");
-    }
-
+        // console.log(".footer_nav>li");
+        if($(this).children(".drop_down").hasClass("drop_down_b")){
+            $(this).children(".drop_down").removeClass("drop_down_b");
+            $(this).children("div").css("border-bottom","1px solid #3D3F4A");
+            $(this).removeClass("img_xz");
+        }else{
+            $(this).children(".drop_down").addClass("drop_down_b").parent().siblings().children(".drop_down").removeClass("drop_down_b");;
+            $(this).children("div").css("border-bottom","transparent");
+            $(this).addClass("img_xz").siblings().removeClass("img_xz");
+        }
     })
   
 })();
